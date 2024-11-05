981beebc292872b26d85ee2fd25da6703b94caa8
Marisela2005 <mariselavincenty@gmail.com>
2024-11-04 22:59:53 -0700

    Registrar clientes

---
+++ SistemaVenta/src/Modelo/ClienteDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dell i7
+ */
+public class ClienteDao {
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    
+    public boolean RegistrarCliente(Cliente cl) {
+        String sql = "INSERT INTO clientes (dni, nombre, telefono, direccion, razon) VALUES (?,?,?,?,?)";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cl.getDni());
+            ps.setString(2, cl.getNombre());
+            ps.setInt(3, cl.getTelefono());
+            ps.setString(4, cl.getDireccion());
+            ps.setString(5, cl.getRazon());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+}
